065460883bd79bafcbac3ff81abda9376d33e72f
Zach Lieberman <zacharylieberman1@gmail.com>
2022-03-26 23:33:56 -0400

    form

--- edapp/urls.py
+++ edapp/urls.py
@@ -17,10 +17,9 @@ from django.urls import include, path, re_path
 from django.contrib import admin
 from . import views
 from django.views.generic.base import TemplateView
-from .views import SignUpView
 
 urlpatterns = [
     path('home/', views.index, name="home"),
-    path('signup/', SignUpView.as_view(), name="signup"),
+    path('signup/', views.signup, name="signup"),
     
 ]
